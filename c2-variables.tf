61c3511d415999ca5660d3faba17e481f05aa380
Tamandeep Singh <tamandeepsinghkhamba@gmail.com>
2024-05-14 15:18:58 -0700

    Update c2-variables.tf

--- c2-variables.tf
+++ c2-variables.tf
@@ -8,3 +8,13 @@ variable "instance_type" {
   description = "EC2 Instance Type - Instance Sizing"
   type = string
 }
+
+variable "aws_access_key_id" {
+  description = "access key"
+  type = string
+}
+
+variable "aws_secret_access_key" {
+  description = "aws_secret_access_key"
+  type = string
+}
